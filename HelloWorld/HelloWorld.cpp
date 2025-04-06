8d0329129e762620daf3fc96094912f44fbab692
Rin <84992619+Rin-coder69@users.noreply.github.com>
2025-04-06 15:25:36 -0600

    kotlin copyEdit Employee class for payroll program

--- HelloWorld/HelloWorld.cpp
+++ HelloWorld/HelloWorld.cpp
@@ -1,8 +1,16 @@
 #include "Header.h"
 #include <iostream>
+#include <string>
 using namespace std;
 
-class Student;
+class Person {
+public:
+    float weight;
+	std::string name;
+	int age;
+    void f(){}
+};
+
 int main() {
 
     Student student("Nick");
@@ -11,8 +19,22 @@ int main() {
     f();
     student.Write();
    
+    Person person;
+    person.age = 19;
+    unsigned int i =  78;
+    //printf("%d\n", i);
+	cout << i << endl;
+	cout << sizeof(int) << endl;
     cout << "Hello, World!" << endl;
     cout << "Git is now tracking this project!" << endl;
+
+    char c = 'A';
+	cout << (int)c << endl;
+    f();
+	std::cout << "Hello World!\n";
+
+
+
     return 0;
 }
 
